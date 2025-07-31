1af7b3952cae70f5c62f20808c10d819db463e23
Mark Adonteng <markadonteng500@gmail.com>
2025-07-31 12:30:10 +0000

    firebase done

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJhJhJhJhJhJhJhJhJhJhJhJhJhJhJhJh",
+  apiKey: "AIzaSyAIRr_HPQZU9rPn59B9KkqSapVMhg2rpUs",
   authDomain: "digifar-3e9e3.firebaseapp.com",
   projectId: "digifar-3e9e3",
   storageBucket: "digifar-3e9e3.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdefghijklmnopqrstuvwxyz",
-  measurementId: "G-ABCDEFGHIJ"
+  messagingSenderId: "291373781867",
+  appId: "1:291373781867:web:bf153e60e6f2b0c9fd322e",
+  measurementId: "G-8VQDBKDD9X"
 };
 
 // Initialize Firebase
